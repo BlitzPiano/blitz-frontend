43d8638b812ca159ff2d46c8feb843000d2bcf53
Hri7566 <hri7566@gmail.com>
2022-12-06 09:09:47 -0500

    fix background color

--- src/BackgroundColor.ts
+++ src/BackgroundColor.ts
@@ -6,7 +6,7 @@ gClient.on('ch', (msg: any) => {
     const color = new Color(msg.ch.settings.color);
     const color2 = new Color(msg.ch.settings.color2 || msg.ch.settings.color);
     
-    if (!msg.ch.settings.color2) color2.add(0x40, 0x40, 0x40);
+    if (!msg.ch.settings.color2) color2.add(-0x40, -0x40, -0x40);
     document.body.style.background = `radial-gradient(circle, ${color.toHexa()} 0%, ${color2.toHexa()} 100%)`;
 
     let bottom = document.getElementById('bottom');
